bab4a84a02c80bf19dcfbd14fb6cf718d3e6f39c
UzairTajdar <126019289+UzairTajdar@users.noreply.github.com>
2024-01-20 23:30:26 +0500

    Create 01_array.js
    learning

---
+++ 02_basic/01_array.js
@@ -0,0 +1,52 @@
+// =====Array=====
+
+// const arr = [9 ,4,5,7,8]
+// console.log(arr[0]);
+// console.log(arr[4]);
+
+// const arr1 = new Array(2,3,4,5,6);
+// const arr1 = ["Uzair" ,"RDJ","Cris even",7,8]
+// console.log(arr1[2]);
+
+
+// ==============Array Methods===================
+
+const num = [2,3,6,5,9,3]
+
+// num.push(19);
+// num.push(7);
+// num.pop()
+// num.pop()
+// num.pop()
+
+// num.unshift(10)              //same as push but insert at first
+// num.unshift(90)
+
+// num.shift()                  //same as push but insert at first
+// num.shift()
+// console.log(num);
+
+console.log(num.includes(9))
+console.log(num.indexOf(9))
+
+
+// console.log(num.join())                 // Make it string
+console.log(typeof num.join())
+
+
+// ====================slice   splice ===================
+
+// const array = [2,3,4,5,6,7,8]
+// console.log("A " ,array)
+
+// // slice 
+// const arr_n1 = array.slice(1,4)
+// console.log("B " ,arr_n1)
+// console.log("A " ,array)
+
+// // splice 
+// const arr_n2 = array.splice(1,4)
+// console.log("C " ,arr_n1)
+// console.log("a " ,array)
+
+

7d44a1d25471fc019985aa1d773b6d992697af4b
UzairTajdar <126019289+UzairTajdar@users.noreply.github.com>
2024-01-21 00:10:24 +0500

    Create 02_array.js

---
+++ 02_basic/02_array.js
@@ -0,0 +1,29 @@
+
+const marvel_heros = ["Ironman","Thor","Captain ","Loki"]
+const DC_heros = ["Superman","wander Girl","Batman"]
+
+// console.log(marvel_heros);
+// marvel_heros.push(DC_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[4][1]);
+
+// const allHeros = marvel_heros.concat(DC_heros)
+// console.log(allHeros);
+
+// const Heros = [...marvel_heros,...DC_heros]           //Another way to cancat more than two also 
+// console.log(Heros);
+
+
+// const nested_Arr = [1,2,3,4 ,[5,6] ,7,8,[4,2,5,[2,3,2]]]
+// console.log(nested_Arr.flat(Infinity));
+
+console.log(Array.isArray("Uzair"))
+console.log(Array.from("Uzair"))
+console.log(Array.from(Object.entries({name:"Uzair"})))
+
+// const score_1 = 100
+// const score_2 = 200
+// const score_3 = 300
+
+// console.log(Array.of(score_1,score_2,score_3));

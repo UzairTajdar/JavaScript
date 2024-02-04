a14e022a301e021a988dbd019fc605921676eaa9
UzairTajdar <126019289+UzairTajdar@users.noreply.github.com>
2024-02-04 19:01:54 +0500

    add_fat_results

--- 07_projects/BMI_Calculator/script.js
+++ 07_projects/BMI_Calculator/script.js
@@ -6,6 +6,7 @@ form.addEventListener('submit', function (e) {
     const height = parseInt(document.querySelector('#height').value);
     const weight  = parseInt(document.querySelector('#weight').value);
     const result = document.querySelector('#results');
+    const fat_result = document.querySelector('#fat_results');
 
     if (height === '' || height < 0 || isNaN(height)) {
         result.innerHTML = 'Please provide a valid height';
@@ -16,5 +17,18 @@ form.addEventListener('submit', function (e) {
     else {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
         result.innerHTML = `<span>Your bmi is ${bmi}</span>`
+
+        if (bmi < 18.5) {
+            fat_result.innerHTML = 'You are underweight';           
+        }
+        else if (bmi >= 18.5 && bmi <= 24.9) {
+            fat_result.innerHTML = 'You are normal weight';
+        }
+        else {
+            fat_result.innerHTML = 'You are overweight';
+        }
     }
+
+   
+
 });

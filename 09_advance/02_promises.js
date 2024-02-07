5b9be0ddf9d480375484d1f740a3fa2eee7aad6e
UzairTajdar <126019289+UzairTajdar@users.noreply.github.com>
2024-02-07 17:32:11 +0500

    Update 02_promises.js

--- 09_advance/02_promises.js
+++ 09_advance/02_promises.js
@@ -34,23 +34,89 @@
 // promiseThree.then(function (user) {
 //     console.log(user)
 // })
-// =================Promise Four==================
-const promiseFour = new Promise(function(resolve,reject){
-    setTimeout(function () {
-        const error = false;
-        if (!error) {
-            resolve({username:"Uzair" , email: "uzair@gmail.com"})
-        }else{
-            reject('Error : Something wrong')
-        }
-    },1000)
-})
 
-promiseFour.then(function (user) {
-    console.log(user)
-    return user.username
-}).then(function (username) {
-    console.log(username)
-}).catch(function (error) {
-    console.log(error)
-})
+
+// =================Promise Four==================
+// const promiseFour = new Promise(function(resolve,reject){
+//     setTimeout(function () {
+//         const error = false;
+//         if (!error) {
+//             resolve({username:"Uzair" , email: "uzair@gmail.com"})
+//         }else{
+//             reject('Error : Something wrong')
+//         }
+//     },1000)
+// })
+
+// promiseFour.then(function (user) {
+//     console.log(user)
+//     return user.username
+// }).then(function (username) {
+//     console.log(username)
+// }).catch(function (error) {
+//     console.log(error)
+// }).finally(()=> console.log('The finally() is executed .The promise is either resolved or rejected '))
+
+
+// =================Promise Five==================
+// const promiseFive = new Promise(function(resolve,reject){
+//     setTimeout(function () {
+//         let error = true;
+//         if (!error) {
+//             resolve({username:"Javascript" , email: "Java@gmail.com"})
+//         }else{
+//             reject('Error : Js Something wrong')
+//         }
+//     },1000)
+// })
+
+// async function consumedFive(){
+//    try {
+//     const response = await promiseFive
+//     console.log(response)
+//    } catch (error) {
+//     console.log(error)
+//    }
+// }
+// consumedFive()
+
+
+// ======================Fetch Method=============================
+// async function getAllUser(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data  = await response.json();
+//         console.log(data)
+//     } catch (error) {
+//         console.log('E :',error)
+//     }
+// }
+// getAllUser()
+
+
+// ======================Fetch Method=============================
+// fetch('https://api.github.com/users/uzairtajdar')
+// .then((response)=>{
+//     return response.json()
+// }).then((data)=>{
+//     console.log(data)
+// }).catch((error)=> console.log(error))
+
+
+// ================ Promise.all()================
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(function () {
+                const error = false;
+                if (!error) {
+                    resolve({username:"Uzair" , email: "uzair@gmail.com"})
+                }else{
+                    reject('Error : Something wrong')
+                }
+            },1000)
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
